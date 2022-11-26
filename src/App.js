fcbad6dda4731cf3435e61e809f3df2f2ad73be0
Brianna Foley <brianna.foley87@gmail.com>
2022-11-25 18:02:47 -0600

    styling complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='app'>
+      <div className='grid'>
+        <div id='display'>
+          <input type='text' />
+        </div>
+        <div id='clear' className='padButton'>AC</div>
+        <div id='divide' className='padButton operator'>/</div>
+        <div id='multiply' className='padButton operator'>x</div>
+        <div id='seven' className='padButton number'>7</div>
+        <div id='eight' className='padButton number'>8</div>
+        <div id='nine' className='padButton number'>9</div>
+        <div id='subtract' className='padButton operator'>-</div>
+        <div id='four' className='padButton number'>4</div>
+        <div id='five' className='padButton number'>5</div>
+        <div id='six' className='padButton number'>6</div>
+        <div id='add' className='padButton operator'>+</div>
+        <div id='one' className='padButton number'>1</div>
+        <div id='two' className='padButton number'>2</div>
+        <div id='three' className='padButton number'>3</div>
+        <div id='equals' className='padButton'>=</div>
+        <div id='zero' className='padButton number'>0</div>
+        <div id='decimal' className='padButton number'>.</div>
+      </div>
     </div>
   );
 }
